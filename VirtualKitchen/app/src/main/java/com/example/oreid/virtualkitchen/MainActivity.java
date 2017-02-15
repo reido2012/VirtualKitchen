a322b03417cdfd4e1638a95725efeca7ec408de9
Omar Reid <or267@bath.ac.uk>
2017-02-15 21:43:54 +0000

    Added Registration and sign in from email

--- VirtualKitchen/app/src/main/java/com/example/oreid/virtualkitchen/MainActivity.java
+++ VirtualKitchen/app/src/main/java/com/example/oreid/virtualkitchen/MainActivity.java
@@ -4,9 +4,11 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -35,12 +37,17 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
     private FirebaseAuth.AuthStateListener mAuthListener;
     private GoogleApiClient mGoogleApiClient;
 
+
     private static final String TAG = "MainActivity";
     private static final int RC_SIGN_IN = 9001;
 
+    EditText emailField;
+    EditText passwordField;
     SignInButton signInButton;
     TextView statusTextView;
     Button signOutButton;
+    Button emailSignIn;
+    Button emailRegister;
 
 
     @Override
@@ -82,6 +89,16 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
 
         signOutButton = (Button) findViewById(R.id.sign_out_button);
         signOutButton.setOnClickListener(this);
+
+        emailSignIn = (Button) findViewById(R.id.email_sign_in_button);
+        emailSignIn.setOnClickListener(this);
+
+        emailRegister = (Button) findViewById(R.id.registration_button);
+        emailRegister.setOnClickListener(this);
+
+
+        emailField = (EditText) findViewById(R.id.email_edit_text);
+        passwordField = (EditText) findViewById(R.id.password_edit_text);
     }
 
     @Override
@@ -98,9 +115,96 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
         }
     }
 
+    private void createAccount(String email, String password){
+        Log.d(TAG, "createAcccount:" + email);
+
+        if(!validateForm()){
+            return;
+        }
+
+        // [START create_user_with_email]
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "createUserWithEmail:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(MainActivity.this, R.string.auth_failed,
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        // [START_EXCLUDE]
+//                        hideProgressDialog();
+                        // [END_EXCLUDE]
+                    }
+                });
+        // [END create_user_with_email]
+    }
+
+    private Boolean validateForm(){
+        boolean valid = true;
+
+        String email = emailField.getText().toString();
+        if (TextUtils.isEmpty(email)) {
+            emailField.setError("Required.");
+            valid = false;
+        } else {
+            emailField.setError(null);
+        }
+
+        String password = passwordField.getText().toString();
+        if (TextUtils.isEmpty(password)) {
+            passwordField.setError("Required.");
+            valid = false;
+        } else {
+            passwordField.setError(null);
+        }
+
+        return valid;
+    }
+
     private void signIn(){
         Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
         startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    private void emailSignIn(String email, String password){
+        Log.d(TAG, "signIn:" + email);
+        if (!validateForm()) {
+            return;
+        }
+
+//        showProgressDialog();
+
+        // [START sign_in_with_email]
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "signInWithEmail:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "signInWithEmail:failed", task.getException());
+                            Toast.makeText(MainActivity.this, R.string.auth_failed,
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        // [START_EXCLUDE]
+                        if (!task.isSuccessful()) {
+                            statusTextView.setText(R.string.auth_failed);
+                        }
+//                        hideProgressDialog();
+                        // [END_EXCLUDE]
+                    }
+                });
+        // [END sign_in_with_email]
     }
     
     public void onActivityResult(int requestCode, int resultCode, Intent data){
@@ -182,8 +286,33 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
             case R.id.sign_out_button:
                 signOut();
                 break;
+            case R.id.registration_button:
+                createAccount(emailField.getText().toString(), passwordField.getText().toString());
+                break;
+            case R.id.email_sign_in_button:
+                emailSignIn(emailField.getText().toString(), passwordField.getText().toString());
+                updateUser();
+                break;
         }
     }
 
+    private void updateUser() {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            // Name, email address, and profile photo Url
+            String name = user.getDisplayName();
+            String email = user.getEmail();
+            String uid = user.getUid();
+
+            // The user's ID, unique to the Firebase project. Do NOT use this value to
+            // authenticate with your backend server, if you have one. Use
+            // FirebaseUser.getToken() instead.
+            if(name == null){
+                statusTextView.setText("Hello, " + email);
+            }else{
+                statusTextView.setText("Hello, " + name);
+            }
+        }
+    }
 
 }
