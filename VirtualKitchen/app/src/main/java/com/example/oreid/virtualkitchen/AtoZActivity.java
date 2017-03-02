7c1bcdcb37136143f3358f8b58f721326ad01a82
Bdour Al Lawzi <ballawzi97@gmail.com>
2017-03-02 12:05:35 +0000

    Revert "Revert "commit myself""
    This reverts commit c8416bcf44566170a21f5914693f56fd1cdcf73b.

---
+++ VirtualKitchen/app/src/main/java/com/example/oreid/virtualkitchen/AtoZActivity.java
@@ -0,0 +1,17 @@
+package com.example.oreid.virtualkitchen;
+
+/**
+ * Created by Bdour on 18-Feb-17.
+ */
+import android.app.Activity;
+import android.os.Bundle;
+
+// same display but different content from the database
+// items that are in the a to z
+
+public class AtoZActivity extends Activity{
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kitchen);
+    }
+}
---
+++ VirtualKitchen/app/src/main/java/com/example/oreid/virtualkitchen/KitchenActivity.java
@@ -0,0 +1,14 @@
+package com.example.oreid.virtualkitchen;
+
+/**
+ * Created by Bdour on 18-Feb-17.
+ */
+import android.app.Activity;
+import android.os.Bundle;
+
+public class KitchenActivity extends Activity{
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kitchen);
+    }
+}
