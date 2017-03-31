9e35134c2fcfe04d3bf4ffcf1588f5efbd90c43a
Hollie Baker <hollieabaker@gmail.com>
2017-03-31 15:49:09 +0100

    Fixed even more merge conflicts - going back to when barcodes worked

--- VirtualKitchen/app/src/main/java/com/example/oreid/virtualkitchen/KitchenTab.java
+++ VirtualKitchen/app/src/main/java/com/example/oreid/virtualkitchen/KitchenTab.java
@@ -31,14 +31,11 @@ public class KitchenTab extends AppCompatActivity implements HasListView {
 
     protected FoodStorageData db = VKData.getInstance().getFoodDB();
 
-    static final int REQUEST_CODE=1;
-    protected FoodStorageData db;
-
-
     private int contentViewId = R.layout.activity_kitchen_tab;
     private int listViewId  = R.id.list_view;
     private String tabName = "Kitchen";
     private StorageArea storageArea = null;
+    public static final int REQUEST_CODE = 5;
 
     private ArrayList<FoodItem> listData = new ArrayList<FoodItem>();
 
@@ -82,57 +79,8 @@ public class KitchenTab extends AppCompatActivity implements HasListView {
         listAdapter.notifyDataSetChanged();
     }
 
-<<<<<<< HEAD
 
-    public void shoppingListHandler(View v) {
 
-        // index of where the item can be found in the list's data.
-        int position = listView.getPositionForView((View) v.getParent());
-
-        db.addToShoppingList(position, this.storageArea);
-
-        updateUI();
-    }
-
-    // called when add button is pressed.
-    // TODO hollie 22/02/17 this should lead to a new intent where the food can be created properly.
-    public void addFood(View v) {
-        Intent intentItem = new Intent(KitchenTab.this, AddItem.class);
-        startActivityForResult(intentItem,REQUEST_CODE);
-       /* final EditText txtField = new EditText(this);
-        AlertDialog dialog = new AlertDialog.Builder(this)
-                .setTitle("Add an item.")
-                .setMessage("Name of item to add to the " + tabName)
-                .setView(txtField)
-                .setPositiveButton("Add", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        String name = String.valueOf(txtField.getText());
-                        db.add(new FoodItem(name, 1, storageArea, 3));
-                    }
-                })
-                .setNegativeButton("Cancel", null)
-                .create();
-        dialog.show();
-*/
-
-    }
-=======
-
->>>>>>> parent of 918f51a... Changed how storage area lists are updated
-
-    @Override
-    protected void onActivityResult (int requestCode, int resultCode, Intent data){
-        if(requestCode==1){
-            if (resultCode == Activity.RESULT_OK){
-                String name = data.getStringExtra("NAME");
-                String quan = data.getStringExtra("QUAN");
-                String expiry = data.getStringExtra("EXP");
-                db.add(new FoodItem (name,Integer.parseInt(quan),storageArea,Integer.parseInt(expiry)));
-                updateUI();
-            }
-        }
-    }
     public void deleteButtonHandler(View v) {
 
         // index of where the item can be found in the list's data.
